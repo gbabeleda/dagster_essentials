6bc15cae567cc17a0bac1aac0921c61938bcda94
Gio Abeleda <gioabeleda@gmail.com>
2024-07-24 03:40:44 +0800

    small comments

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -5,15 +5,20 @@ from dagster import asset
 # comments for myself its been awhile since i python-ed
 @asset
 def taxi_trips_file() -> None: # type annotation
+    # docstring
+    # displays in the asset description in the global asset lineage
     '''
         The raw parquet files for the taxi trips dataset. Source from NYC Open Data portal 
-    ''' # docstring
+    ''' 
 
 
     month_to_fetch = '2023-03'
+
+    # fstring and http requests
     raw_trips = requests.get(
-        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet" # fstring and http requests
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet" 
     )
 
-    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), 'wb') as output_file: # file io
+    # file io
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), 'wb') as output_file: 
         output_file.write(raw_trips.content)
