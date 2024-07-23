3047c500e003fc98473ac5c49a9f7e6d983a0d26
Gio Abeleda <gioabeleda@gmail.com>
2024-07-24 03:20:20 +0800

    added comments

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -3,7 +3,7 @@ from . import constants
 from dagster import asset 
 
 @asset
-def taxi_trips_file() -> None: 
+def taxi_trips_file() -> None: # type annotation
     '''
         The raw parquet files for the taxi trips dataset. Source from NYC Open Data portal
     '''
@@ -11,8 +11,8 @@ def taxi_trips_file() -> None:
 
     month_to_fetch = '2023-03'
     raw_trips = requests.get(
-        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet" # fstring and http requests
     )
 
-    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), 'wb') as output_file:
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), 'wb') as output_file: # file io
         output_file.write(raw_trips.content)
